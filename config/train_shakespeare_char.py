6b174936773147d804f0ebe265b716426f51b517
Akshay Kolli <proflepton@gmail.com>
2023-10-08 13:42:43 -0400

    Train iters change

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -25,7 +25,7 @@ n_embd = 1024
 dropout = 0.2
 
 learning_rate = 1e-4 # with baby networks can afford to go a bit higher
-max_iters = 5000
+max_iters = 10_000
 lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-6 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
